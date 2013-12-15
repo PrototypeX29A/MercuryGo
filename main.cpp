1977f144a6677649a56e8b27a614624032936bfd
Peter <preineke@gmx.com>
2013-12-15 21:49:58 +0100

    fly mercury through a world of cga

--- main.cpp
+++ main.cpp
@@ -12,7 +12,9 @@ SDL_Surface *screen = NULL;
 void put_pixel(SDL_Surface *surface, int x, int y, Uint32 pixel)
 {
     Uint32 *pixels = (Uint32 *)surface->pixels;
-    pixels[ ( y * surface->w ) + x ] = pixel;
+    int w = surface->w;
+    Uint32 *target = &pixels[ ( y * w) + x ];
+    *target = pixel;
 }
 
 SDL_Surface *load_sprite(std::string filename)
@@ -38,7 +40,7 @@ void draw_sprite(SDL_Surface* target, SDL_Surface* image, int x, int y)
     offset.x = x;
     offset.y = y;
 
-    SDL_BlitSurface( image, NULL, target, &offset );
+    SDL_BlitSurface(image, NULL, target, &offset);
 }
 
 bool init()
@@ -73,24 +75,28 @@ int main()
     }
     map_part* map = new map_part();
 
-    for (int x=0; x<640; x++)
-        for (int y=0; y<480; y++) {
 
-        put_pixel(screen, x, y, map->get_pixel(x,y));
-
-    }
-    draw_sprite(screen, ship, 320, 240);
-
-
-    if (SDL_Flip(screen)==-1)
-    {
-        return 1;
-    }
 
     SDL_Event event;
 
+    uint32_t last_game_tick = SDL_GetTicks();
+    uint32_t ship_y = 0x10000;
     while (!quit)
     {
+        for (int x=0; x<640; x++)
+            for (int y=0; y<480; y++) {
+
+            put_pixel(screen, x, y, map->get_pixel(x,y+ship_y));
+
+        }
+        draw_sprite(screen, ship, 320, 240);
+
+
+        if (SDL_Flip(screen)==-1)
+        {
+            return 1;
+        }
+
         while (SDL_PollEvent(&event))
         {
             if (event.type==SDL_QUIT)
@@ -98,6 +104,8 @@ int main()
                 quit = true;
             }
         }
+        SDL_Delay(20);
+        ship_y-=4;
     }
 
     SDL_FreeSurface(ship);
--- src/map_part.cpp
+++ src/map_part.cpp
@@ -12,7 +12,7 @@ map_part::~map_part()
 
 int map_part::get_pixel(int x, int y)
 {
-    if ((x < 270) || (x>370))
-        return ((x+y) % 2 ? 0x00000000 : ((y / 32) % 2 ? 0x00FF00FF : 0x0000FFFF));
-    return 0xFFFFFFFF;
+    if ((x < 220) || (x>420))
+        return ((x/2 + y/2) % 2 ? 0x00000000 : ((y / 32) % 2 ? 0x00FF00FF : 0x0000FFFF));
+    return ((x/2 + y/2) % 2 ? 0xFFFFFFFF : 0x00000000);
 }
